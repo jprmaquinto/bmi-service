1f903874b30c9c26d1a87d03691f51b7e947baf8
jprmaquinto <jprmaquinto@gmail.com>
2020-07-10 20:24:41 +0800

    Initial Commit

---
+++ src/main/java/com/example/service/bmi/BmiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.service.bmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmiServiceApplication.class, args);
+	}
+
+}
